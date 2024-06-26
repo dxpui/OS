3fa72e0be35c42161d79d0b74705d553347a6006
alokuiux <alok.ranjan@ofcom.org.uk>
2024-06-26 07:43:47 +0100

    Updated page

--- js/main.js
+++ js/main.js
@@ -20,3 +20,45 @@ $(document).ready(function () {
     }
 });
 
+$(document).ready(function() {
+    $('.checker-container').each(function() {
+        $(this).parents().addClass('height-100');
+    });
+
+    if ($('.checker-container').length) {
+        // $('body').addClass('position-relative');
+        $('header').addClass('position-absolute');
+    }
+
+    // $('.checker-container-left-inner').each(function() {
+    //     var $this = $(this);
+    //     if ($this[0].scrollHeight > $this.innerHeight() || $this[0].scrollWidth > $this.innerWidth()) {
+    //         $this.addClass('height-auto');
+    //     }
+    // });
+});
+
+$(document).ready(function() {
+    function checkScroll() {
+        $('.checker-container').each(function() {
+            var topHeader = $('.header').height();
+            var footerHeight  = $('.qn-stepper').height();
+            var checkerContainer  = $('.checker-container').height();
+
+            var $firstDiv = $(this);
+            var $secondDiv = $firstDiv.find('.checker-container-left-inner');
+
+            if (($(document).height() - 10) < (topHeader + footerHeight + checkerContainer) && $(window).width() < 1280) {
+                $secondDiv.addClass('height-auto');
+            } else {
+                $secondDiv.removeClass('height-auto');
+            }
+        });
+    }
+
+    checkScroll();
+
+    $(window).resize(function() {
+        checkScroll();
+    });
+});
